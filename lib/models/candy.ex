041d8c1c4d9ba2c52abcd2cdbbf8a0ca5ac06965
feliks.pobiedzinski@swmansion.com <feliks.pobiedzinski@swmansion.com>
2024-08-14 16:24:48 +0200

    Do everything except adding info about membrane

--- lib/models/candy.ex
+++ lib/models/candy.ex
@@ -12,16 +12,20 @@ defmodule Workshop.Models.Candy do
     )
   end
 
-  @spec postprocess_rescale(Nx.Tensor.t()) :: Nx.Tensor.t()
-  def postprocess_rescale(tensor) do
+  @spec postprocess(Nx.Tensor.t()) :: Nx.Tensor.t()
+  def postprocess(tensor) do
     standard_deviation = Nx.variance(tensor) |> Nx.to_number() |> :math.sqrt()
     scale = 255 / (5.5 * standard_deviation)
     shift_tensor = Nx.broadcast(0.7 * standard_deviation, Nx.shape(tensor))
 
     tensor
+    |> Nx.backend_transfer(EXLA.Backend)
     |> Nx.add(shift_tensor)
     |> Nx.multiply(scale)
     |> Nx.max(0)
     |> Nx.min(255)
+    |> Nx.round()
+    |> Nx.as_type(:u8)
+    |> Nx.to_binary()
   end
 end
